c375115b52b0342b7cb21b57c3be2bf0d13af0e4
Anton K <antonk@MacBook-Air-Anton.local>
2022-05-04 18:19:18 +0300

    select element

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types"
 
-const GroupList = ({items, valueProperty, contentProperty}) => {
+const GroupList = ({items, valueProperty, contentProperty, onItemSelect, selectedItem}) => {
     console.log(Object.keys(items));
     return (
         <ul className="list-group">
             {Object.keys(items).map((item) => (
-                <li key={items[item][valueProperty]} className="list-group-item">
+                <li key={items[item][valueProperty]} 
+                className={"list-group-item" + (items[item] === selectedItem ? " active" : "")}
+                onClick={() => onItemSelect(items[item])} 
+                role="button"
+                >
                     {items[item][contentProperty]}
                 </li>
             ))}
@@ -20,6 +24,8 @@ GroupList.defaultProps={
 GroupList.propTypes = {
     items:           PropTypes.object.isRequired,
     valueProperty:   PropTypes.string.isRequired,
-    contentProperty: PropTypes.string.isRequired
+    contentProperty: PropTypes.string.isRequired,
+    onItemSelect:    PropTypes.func,
+    selectedItem:    PropTypes.object
 };
 export default GroupList;
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -9,14 +9,15 @@ import GroupList from "./groupList";
 const Users = ({ users, ...rest }) => {
     const [currentPage, setCurrentPage] = useState(1)
     const [professions, setProfessions]=useState();
+    const [selectedProf, setSelectedProf]=useState()
 
     const count = users.length
     const pageSize = 4
     useEffect(() => {
         api.professions.fetchAll().then((data) => setProfessions(data))
     }, []);
-    const handleProfessionSelect = (params) => {
-        console.log(params)
+    const handleProfessionSelect = item => {
+        setSelectedProf(item)
     };
     
         // console.log(professions);
@@ -27,9 +28,10 @@ const Users = ({ users, ...rest }) => {
     return (
         <>
           {professions && (
-            <GroupList items={professions} onItemSelect = {handleProfessionSelect}
-            valueProperty="_id"
-            contentProperty="name"
+            <GroupList 
+            selectedItem={selectedProf}
+            items={professions} 
+            onItemSelect = {handleProfessionSelect}
             />
           )}
             {count > 0 && (
