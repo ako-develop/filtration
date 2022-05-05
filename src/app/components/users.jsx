9e312a01202de17dd7ba2ef07a1f225dd8cc2e0f
Anton K <antonk@MacBook-Air-Anton.local>
2022-05-05 17:08:24 +0300

    refactoring all error empty

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -5,17 +5,21 @@ import User from "./user";
 import api from "../api"
 import PropTypes from "prop-types"
 import GroupList from "./groupList";
+import SearchStatus from "./searchStatus"
 
 const Users = ({ users: allUsers, ...rest }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfessions]=useState();
     const [selectedProf, setSelectedProf]=useState();
 
-    const count = allUsers.length
-    const pageSize = 4
+  
+    const pageSize = 2
     useEffect(() => {
         api.professions.fetchAll().then((data) => setProfessions(data));
     }, []);
+    useEffect(()=>{
+        setCurrentPage(1)
+    }, [selectedProf])
     const handleProfessionSelect = item => {
         setSelectedProf(item)
     };
@@ -27,23 +31,30 @@ const Users = ({ users: allUsers, ...rest }) => {
     const filteredUsers = selectedProf 
         ? allUsers.filter((user) => user.profession === selectedProf) 
         : allUsers;
-
+        const count = filteredUsers.length
     const userCrop = paginate(filteredUsers, currentPage, pageSize);
     const clearFilter = () => {setSelectedProf()};
 
     return (
-        <>
+        
+        <div className="d-flex">
+         
           {professions && (
-              <>
+              <div className="d-flex flex-column flex shrink-0 p-3">
+              
             <GroupList 
             selectedItem={selectedProf}
             items={professions} 
             onItemSelect = {handleProfessionSelect}
             />
             <button className="btn btn-secondary mt-2" onClick={clearFilter}>Очистить</button>
-            </>
+            
+            </div>
           )}
+          <div className="d-flex flex-column">
+          <SearchStatus length={count} />
             {count > 0 && (
+                
                 <table className="table">
                     <thead>
                         <tr>
@@ -66,15 +77,20 @@ const Users = ({ users: allUsers, ...rest }) => {
                         ))}
                     </tbody>
                 </table>
+                
             )}
-            
+             <div className="d-flex justify-content-center">
             <Pagination
                 itemsCount={count}
                 pageSize={pageSize}
                 currentPage={currentPage}
                 onPageChange={handlePageChange}
             />
-        </>
+            </div>
+            </div>
+           
+        </div>
+        
     )
 }
 
